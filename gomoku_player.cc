1e96382765a12d8f4f8b01d5ca015ca6c0a7e129
Bruno Marques <brunomn95@gmail.com>
2017-08-20 16:14:30 -0300

    adding player class

---
+++ gomoku_player.cc
@@ -0,0 +1,28 @@
+#include "gomoku_player.h"
+
+GomokuPlayer::GomokuPlayer()
+: _player_type(HUMAN)
+{}
+
+GomokuPlayer::~GomokuPlayer()
+{}
+
+void GomokuPlayer::play(int x, int y)
+{
+  _plays.insert(GomokuPlay(x,y));
+}
+
+bool GomokuPlayer::already_played(int x, int y)
+{
+  auto aux = _plays.find(GomokuPlay(x,y));
+  
+  if(aux != _plays.end())
+    return true;
+
+  return false;
+}
+
+std::unordered_set<GomokuPlay> GomokuPlayer::plays()
+{
+  return _plays;
+}
---
+++ gomoku_player.h
@@ -0,0 +1,26 @@
+#ifndef GOMOKU_PLAYER_H
+#define GOMOKU_PLAYER_H
+
+#include <unordered_set>
+#include <iterator>
+#include "gomoku_play.h"
+
+
+enum PlayerType{ 
+  HUMAN = 0,
+  COMPUTER = 1,
+};
+
+class GomokuPlayer {
+public:
+	GomokuPlayer();
+	~GomokuPlayer();
+  void play(int x, int y);
+  bool already_played(int x, int y);
+  std::unordered_set<GomokuPlay> plays();
+private:
+  PlayerType _player_type;
+  std::unordered_set <GomokuPlay> _plays;
+};
+
+#endif //GOMOKU_PLAYER_H
