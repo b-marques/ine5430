8c45e8c42a7c33b3820ad6c284bbcd80ce61304f
Bruno Marques <brunomn95@gmail.com>
2017-08-20 16:15:34 -0300

    tests with unordered_set

--- main.cc
+++ main.cc
@@ -4,8 +4,27 @@
 #include <iterator>
 #include <iostream>
 
+#include <unordered_set>
+#include "gomoku_player.h"
+
 int main (int argc, char *argv[])
 {
+
+  // GomokuPlayer player = GomokuPlayer();
+
+  // std::cout <<  "set contains:" << std::endl;
+  // player.play(0, 0);
+
+  // auto aux = player.plays().find(GomokuPlay(0,0));
+  // if(aux != player.plays().end()){
+  //   std::cout << player.plays().size() << std::endl;
+  // } else {
+  //   std::cout << "not find" << std::endl;
+  // }
+
+  // for (auto it = player.plays().begin(); it != player.plays().end(); ++it)
+  //   std::cout << " " << it -> x();
+ 
   auto app = Gtk::Application::create(argc, argv, "ine5430.gomoku");
   
   MainWindow main_window;
